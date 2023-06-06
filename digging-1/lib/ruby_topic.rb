26ce86bc76e5fa678294016b237b8283352f4f54
noracami <kerke.2011@gmail.com>
2023-06-06 19:58:08 +0800

    1st pass atdd

--- digging-1/lib/ruby_topic.rb
+++ digging-1/lib/ruby_topic.rb
@@ -1,5 +1,28 @@
 module ShareHub
   class RubyTopic
-    def initialize(name:); end
+    attr_accessor *%i[name approve category comments]
+    def initialize(name:)
+      @name = name
+      @approve = false
+      @category = 'ruby'
+      @comments = []
+    end
+
+    def approve_topic
+      @approve = true
+    end
+
+    def add_comment(comment)
+      @comments << "ruby: " + comment.to_s
+    end
+
+    def list_comments
+      @comments.map.with_index do |comment, idx|
+        {
+          content: comment,
+          no: idx+1
+        }
+      end
+    end
   end
 end
--- digging-1/lib/topic.rb
+++ digging-1/lib/topic.rb
@@ -1,5 +1,28 @@
 module ShareHub
   class Topic
-    def initialize(name:); end
+    attr_accessor *%i[name approve category comments]
+    def initialize(name:)
+      @name = name
+      @approve = false
+      @category = 'common'
+      @comments = []
+    end
+
+    def approve_topic
+      @approve = true
+    end
+
+    def add_comment(comment)
+      @comments << comment
+    end
+
+    def list_comments
+      @comments.map.with_index do |comment, idx|
+        {
+          content: comment,
+          no: idx+1
+        }
+      end
+    end
   end
 end
